1579bb0fcd575cb78af7eeb71fc282a8ca471307
daelzikri <daelzik51@gmail.com>
2024-12-15 16:17:00 +0800

    membuat inheritance item

--- Main.java
+++ Main.java
@@ -2,26 +2,30 @@ public class Main {
     public static void main(String[] args) {
         Linkedlist_item item = new Linkedlist_item();
         //setiap level itemnya berbeda untuk player;
-        NodePlayer dael = new NodePlayer("dael", 0, 0, 0, "player");
-        NodeItem Playerlevel_1 = new NodeItem("bero", 0, false);
-        NodeItem.Potion healing = Playerlevel_1.new Potion("healing", 0, false, 0, 0);
-
-        //Menambahkan semua level ke dalam linkedlist
-        item.tambahData(Playerlevel_1);
-        // item.tambahData(Playerlevel_2);
-        // item.tambahData(Playerlevel_3);
-        // item.tambahData(Playerlevel_4);
-        // item.tambahData(Playerlevel_5);
-        // item.tambahData(Playerlevel_6);
-
-
-        // item.tambahData(Monsterlevel_1);
-        // item.tambahData(Monsterlevel_2);
-        // item.tambahData(Monsterlevel_3);
-        // item.tambahData(Monsterlevel_4);
-        // item.tambahData(Monsterlevel_5);
-        // item.tambahData(Monsterlevel_6);
-
-    }
-    
+        NodePlayer pemain = new NodePlayer("pemain", 0, 0, 0, "player");
+        NodeItem Backpack = new NodeItem("ber", 0, false);
+        NodeItem.Weapon weapon_1 = Backpack.new Weapon("Shadowfang", 0, false, 0);
+        NodeItem.Armor armor_1 = Backpack.new Armor("Nightshade Plate", 0, false, 0);
+        NodeItem.Potion potion_1 = Backpack.new Potion("Elixir of Shadows", 0, false, 0, 0);
+        
+        NodeItem.Weapon weapon_2 = Backpack.new Weapon("Crimson Vortex", 0, false, 0);
+        NodeItem.Armor armor_2 = Backpack.new Armor("Ironveil", 0, false, 0);
+        NodeItem.Potion potion_2 = Backpack.new Potion("Lifeblood Brew", 0, false, 0, 0);
+        
+        NodeItem.Weapon weapon_3 = Backpack.new Weapon("Voidsplitter", 0, false, 0);
+        NodeItem.Armor armor_3 = Backpack.new Armor("Emberforge Suit", 0, false, 0);
+        NodeItem.Potion potion_3 = Backpack.new Potion("Etherflask", 0, false, 0, 0);
+       
+        NodeItem.Weapon weapon_4 = Backpack.new Weapon("Arcane Reaver", 0, false, 0);
+        NodeItem.Armor armor_4 = Backpack.new Armor("Frostbound Cloak", 0, false, 0);
+        NodeItem.Potion potion_4 = Backpack.new Potion("Dragonfire Tonic", 0, false, 0, 0);
+       
+        NodeItem.Weapon weapon_5 = Backpack.new Weapon("Stormrend", 0, false, 0);
+        NodeItem.Armor armor_5 = Backpack.new Armor("Shatterguard", 0, false, 0);
+        NodeItem.Potion potion_5 = Backpack.new Potion("Spiritbound Vial", 0, false, 0, 0);
+       
+        NodeItem.Weapon weapon_6 = Backpack.new Weapon("Soulpiercer", 0, false, 0);
+        NodeItem.Armor armor_6 = Backpack.new Armor("Astral Mantle", 0, false, 0);
+        NodeItem.Potion potion_6 = Backpack.new Potion("Crimson Nectar", 0, false, 0, 0);
+    }   
 }
