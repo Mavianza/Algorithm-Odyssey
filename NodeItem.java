cbdbbe9da91fcbe1c9ea0765eca3304ff5f7551a
adhietya <laluadittyapratamajelindra@gmail.com>
2024-12-15 15:14:36 +0800

    perbaiki node item ~adit

--- NodeItem.java
+++ NodeItem.java
@@ -1,20 +1,37 @@
 public class NodeItem {
     NodeItem next = null;
-    String senjata;
+    String namaItem;
     int damage;
-    String armor;
-    int defense;
-    String potion;
-    int healingAmount;
-    String item_khusus;
-
-    NodeItem(String senjata, int damage, String armor, int defense, String potion, int healingAmount, String item_khusus){
-        this.senjata = senjata;
+    boolean item_khusus;
+    
+    NodeItem(String namaItem, int damage, boolean item_khusus) {
+        this.namaItem = namaItem;
         this.damage = damage;
-        this.armor = armor;
-        this.defense = defense;
-        this.potion = potion;
-        this.healingAmount = healingAmount;
         this.item_khusus = item_khusus;
+        this.next = null;
+    }
+    class Weapon extends NodeItem {
+        int power;
+        
+        Weapon(String namaItem, int damage, boolean item_khusus, int power) {
+            super(namaItem, damage,item_khusus);
+            this.power = power;
+        }
+    }
+    
+    class Armor extends NodeItem {
+        int defense;
+        Armor(String namaItem, int damage, boolean item_khusus, int defense) {
+            super(namaItem, damage, item_khusus);
+            this.defense = defense;
+        }
+    }
+    
+    class Potion extends NodeItem {
+        int healingAmount;
+        Potion(String namaItem, int damage, boolean item_khusus, int healingAmount) {
+            super(namaItem, damage, item_khusus);
+            this.healingAmount = healingAmount;
+        }
     }
 }
